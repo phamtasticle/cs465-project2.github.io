d1c86493db790028cc4ea0caddaf7db013f2bffd
vivianh1234 <hvivian@pdx.edu>
2020-03-08 22:34:25 -0700

    fixed buttons

--- content/Search.js
+++ content/Search.js
@@ -116,8 +116,8 @@ function sendGetRequest(q) {
 
     sessionStorage.setItem("food", JSON.stringify(response.hits));
 
-    window.location.pathname = "cs465-project2.github.io/content/page1.html";
-    //window.location.pathname = "content/page1.html";
+    //window.location.pathname = "cs465-project2.github.io/content/page1.html";
+    window.location.pathname = "content/page1.html";
     
   };
 
